faf3aa9730dc83e817b623368f9144c5925a8ea3
Arjan ten Hoeve <arjan.ten.hoeve@achmea.nl>
2020-12-06 18:06:13 +0100

    bijna tweede klaar

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace aoc4
 {
     class Program
     {
-         
+       
         static void Main(string[] args)
         {
             var passport = new Dictionary<string, string>();  //begin met een leeg paspoort. key=het passport veld
@@ -45,20 +45,51 @@ namespace aoc4
         public static bool IsPassportValid(Dictionary<string, string> passport)
         {
             //
-            if (passport.Count == 8)  //alle velden aanwezig
+            if (passport.Count == 8 || (passport.Count == 7 && !passport.ContainsKey("cid")))  //alle velden aanwezig of er mist alleen het cid veld
             {
-                return true;
-            }
-            else
-            {
-                if (passport.Count == 7 && !passport.ContainsKey("cid")) //er mist alleen het veld cid
+                if (passport["byr"].Length != 4) return false;
+                if (int.Parse(passport["byr"]) < 1920 || int.Parse(passport["byr"])> 2002 ) return false;
+                if (passport["iyr"].Length != 4) return false;
+                if (int.Parse(passport["iyr"]) < 2010 || int.Parse(passport["iyr"]) > 2020) return false;
+                if (passport["eyr"].Length != 4) return false;
+                if (int.Parse(passport["eyr"]) < 2020 || int.Parse(passport["eyr"]) > 2030) return false;
+
+                if (!(passport["hgt"].EndsWith("cm") || passport["hgt"].EndsWith("in")))  return false;
+                try
                 {
-                    return true;
+                    int hgt = int.Parse(passport["hgt"].Remove(passport["hgt"].Length - 2));  //haal laaste twee tekens (in of cm eraf) en maak er een int van
+                    if (passport["hgt"].EndsWith("cm"))
+                    {
+                        if(hgt<150 || hgt>193) { return false; }
+                    }
+                    else  //maat is in inch
+                    {
+                        if (hgt < 59 || hgt > 76) { return false; }
+                    }
                 }
-                else
+                catch
                 {
+                    // wat voor de cm of in staat is niet te converteren naar een getal.
                     return false;
                 }
+
+                if (!passport["hcl"].StartsWith('#'))  return false; 
+                if(!Regex.IsMatch(passport["hcl"].Substring(1), "^[a-f0-9]{6}$")) return false;  //a-f of 0-9 precies 6 tekens
+
+                var Eyecolors = new List<string> {"amb","blu","brn","gry","grn","hzl","oth"};
+                if (!Eyecolors.Contains(passport["ecl"])) return false;
+                
+                // nog eenje
+                    
+
+
+                    return true;
+            }
+            else // aantal velden klopt niet
+            {
+                
+               return false;
+               
             }
            
         }
